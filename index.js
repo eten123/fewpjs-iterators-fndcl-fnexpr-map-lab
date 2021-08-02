7e41fd56fae5a1e5058bd94bb24e742131b01628
Eten <eten.uket@yale.edu>
2021-08-02 09:38:08 -0400

    commit tree 26e507ff1e69ef22b5bcba601d4afc6a5deb4774

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  // use the .map method on the tutorials to return a new array
-  return tutorials
+  return tutorials.map(function(string){  // use the .map method on the tutorials to return a new array
+    return string.split(" ").map(word => word[0].toUpperCase() + word.substr(1)).join(" ")  //splits the string at the first space to grab the first word. Then we grab the first character of the word. The word is just a placeholder for the position of the character in the array. Don't forget a word is just an array fo characters
+  })  //takes the first positon at [0] of the word and converts it to upper with the remaining word. the substr returns everything after the 1st 
+
 }
